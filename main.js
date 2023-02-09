7e09c29858c0d6e72dacea43b8d675d7567ef00b
Jordon Perkins <jordon.rubel@gmail.com>
2023-02-09 09:24:07 -0600

    only commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const quarterValue = (orgNum) => {
+    const quarter = orgNum / 4
+    return quarter
+}
+
+// Invoke the function and store the return value
+ const returnValue = quarterValue(10)
+
+// Log the return value
+ console.log(returnValue)
+
+//this function begins with a variable quarterValue and the parameter orgNum
+//in the body of the function there is a varaible quarter that take the argument orgNum divided by 4 and quarter is returned
+//the variable returnValue then stores the varible quarterValue???????
+//log to the console the variable returnValue
